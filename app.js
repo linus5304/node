7681f02524e1ff918d248a0969c29c5f716c1fd3
Linus Bayere <linusbayere@gmail.com>
2021-04-26 07:02:50 +0100

    port variable

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req, res)=>{
     res.send("Welcome to the home page baby")
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("connected")
 });
 
